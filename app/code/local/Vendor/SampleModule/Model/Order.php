f6cc066f37c4415480a41e29fefffeffb1c530d8
abd0m0hamed <abdo.tasks@gmail.com>
2018-08-09 19:20:14 +0200

    First Push.

---
+++ app/code/local/Vendor/SampleModule/Model/Order.php
@@ -0,0 +1,10 @@
+<?php
+
+class Vendor_SampleModule_Model_Order extends Mage_Sales_Model_Order
+{
+    public function queueOrderUpdateEmail($notifyCustomer = true, $comment = '', $forceMode = false)
+    {
+        
+        //My New Code Here
+    }
+}
---
+++ app/code/local/Vendor/SampleModule/controllers/SecurityController.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+class Vendor_SampleModule_SecurityController extends Mage_Core_Controller_Front_Action {
+    
+    
+    function indexAction(){
+    
+        //My Conteoller Code Here
+        
+    }
+}
